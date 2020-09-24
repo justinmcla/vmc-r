77114bf60c365e8f270c94da011b37bd30632824
justinmcla <jwmclawhorn@gmail.com>
2020-09-23 21:20:42 -0400

    fixed users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
