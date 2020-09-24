6ff14f5e1c66d4ca0c103a1c4236560567e57e2e
justinmcla <jwmclawhorn@gmail.com>
2020-09-24 18:38:01 -0400

    built venues controller

---
+++ app/models/venue.rb
@@ -0,0 +1,7 @@
+class Venue < ApplicationRecord
+    validates :name, presence: true
+    validates :seats, presence: true
+    validates :configuration, presence: true
+    validates :greenrooms, presence: true
+    validates :dressing_rooms, presence: true
+end
