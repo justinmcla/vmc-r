233ed8d41ba6d77b59427893c417e60bce1e32b1
justinmcla <jwmclawhorn@gmail.com>
2020-09-24 18:37:30 -0400

    added venue routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :venues, only: [:new, :create]
   get '/home', to: 'admin#home'
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
