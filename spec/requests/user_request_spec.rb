62d96ba640c110201e8c76afa6317a2e22b979c6
justinmcla <jwmclawhorn@gmail.com>
2020-09-23 21:21:20 -0400

    default spec for gen models

---
+++ spec/requests/user_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/user/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/user/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/user/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
