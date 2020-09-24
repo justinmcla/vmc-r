50b9c2a18c4cd9f56f1188f5eb7bf3bf67426519
justinmcla <jwmclawhorn@gmail.com>
2020-09-23 22:17:40 -0400

    built controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def auth
+        redirect_to root_path unless logged_in?
+    end
+
+    def current_user
+        User.find_by_id(session[:user_id])
+    end
 end
