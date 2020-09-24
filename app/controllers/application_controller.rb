a2ed9f8b6c88d956c1949cff0f40b1d1533b8cf2
justinmcla <jwmclawhorn@gmail.com>
2020-09-23 22:18:22 -0400

    moving current_user to views helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,4 @@ class ApplicationController < ActionController::Base
     def auth
         redirect_to root_path unless logged_in?
     end
-
-    def current_user
-        User.find_by_id(session[:user_id])
-    end
 end
