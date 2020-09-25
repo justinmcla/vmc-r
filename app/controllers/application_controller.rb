a55d149289de74ca670647f407056093d12d08fc
justinmcla <jwmclawhorn@gmail.com>
2020-09-25 16:52:24 -0400

    added current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     def auth
         redirect_to root_path unless logged_in?
     end
+
+    def current_user
+        User.find_by_id(session[:user_id])
+    end
 end
