37d63cf6f91337cc9f014cc058b5f5e7dd13a49c
justinmcla <jwmclawhorn@gmail.com>
2020-09-25 22:29:17 -0400

    built address_attr= method

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -2,4 +2,13 @@ class Organizer < ApplicationRecord
     has_and_belongs_to_many :users
     has_one :address, as: :addressable
     accepts_nested_attributes_for :address
+    def address_attributes=(address_attributes)
+        address = Address.find_by_id(address_attributes[:id])
+        if address
+            address.update(address_attributes)
+        else
+            address = Address.create(address_attributes)
+            self.address = address
+        end
+    end
 end
