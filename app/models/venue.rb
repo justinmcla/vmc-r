6cc29ed790e16a85a8dcbd02e10a630a7df0bbe4
justinmcla <jwmclawhorn@gmail.com>
2020-09-25 21:30:05 -0400

    switched to has one

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,12 +5,15 @@ class Venue < ApplicationRecord
     validates :greenrooms, presence: true
     validates :dressing_rooms, presence: true
     belongs_to :user
-    has_many :addresses, as: :addressable
-    accepts_nested_attributes_for :addresses
-    def addresses_attributes=(address_attributes)
-        address_attributes.values.each do |address_attribute|
-          address = Address.find_or_create_by(address_attribute)
-          self.addresses << address
-        end
-      end
+    has_one :address, as: :addressable, dependent: :destroy
+    accepts_nested_attributes_for :address
+    def address_attributes=(address_attributes)
+        address = Address.find_by_id(address_attributes[:id])
+        if address
+            address.update(address_attributes)
+        else
+            address = Address.create(address_attributes)
+            self.address = address
+        end 
+    end
 end
