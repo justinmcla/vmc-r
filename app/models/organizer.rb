d3be418302fcced8f718fa151c0afc22bb074cf4
justinmcla <jwmclawhorn@gmail.com>
2020-09-26 12:19:59 -0400

    added associations

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,6 +1,15 @@
 class Organizer < ApplicationRecord
     validates :name, presence: true
     has_and_belongs_to_many :users
+    has_many :point_of_contacts
+    has_many :contacts, as: :contactable
+    accepts_nested_attributes_for :point_of_contacts, reject_if: :blank_point_of_contact
+    accepts_nested_attributes_for :contacts, reject_if: :blank_contact
+    has_many :contacts, through: :point_of_contacts
     has_one :address, as: :addressable
     accepts_nested_attributes_for :address, reject_if: :blank_address
+
+    def blank_point_of_contact(attributes)
+        attributes[:name].blank?
+    end
 end
