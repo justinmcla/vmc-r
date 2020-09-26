eff04760c55d29cfce3bf6fb8f2bd2824c9dba00
justinmcla <jwmclawhorn@gmail.com>
2020-09-25 21:29:14 -0400

    built update/delete actions

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,13 +1,14 @@
 class VenuesController < ApplicationController
     layout 'admin'
+    before_action :set_venue, only: [:edit, :update, :destroy]
+
     def new
         @venue = Venue.new
-        @venue.addresses.build
+        @venue.build_address
     end
 
     def create
         @venue = current_user.venues.build(post_params)
-        puts post_params
         if @venue.save
             current_user.venues << @venue
             redirect_to home_path
@@ -20,15 +21,27 @@ class VenuesController < ApplicationController
     end
 
     def update
+        @venue.assign_attributes(post_params)
+        if @venue.save
+            redirect_to home_path
+        else
+            redirect_to edit_venue_path
+        end
     end
 
     def destroy
+        @venue.destroy
+        redirect_to home_path
     end
 
     private
 
+    def set_venue
+        @venue = Venue.find_by_id(params[:id])
+    end
+
     def post_params
         params.require(:venue).permit(:name, :configuration, :seats, :greenrooms, :dressing_rooms, 
-                                        addresses_attributes: [:address_1, :address_2, :city, :state, :zip])
+                                        address_attributes: [:id, :address_1, :address_2, :city, :state, :zip])
     end
 end
