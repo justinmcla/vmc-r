29f6af0c10a2bd2e62bdcd52adaaf3d5b0db64c5
justinmcla <jwmclawhorn@gmail.com>
2020-09-25 22:30:12 -0400

    added admin organizer routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   namespace :admin do
-    resources :venues
+    resources :venues, :organizers
   end
   get '/home', to: 'admin#home'
   root 'static#index'
