501593431729f1b985762adee0415b4571e91a7d
justinmcla <jwmclawhorn@gmail.com>
2020-09-26 23:01:29 -0400

    added validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
-    belongs_to :user
+    validates :name, presence: true
+    validates :event_type, presence: true
+    validates :date, presence: true
+    validates :event_time, presence: true
+    validates :access_time, presence: true
+    validates :exit_time, presence: true
+    validates :attendance, presence: true
+    validates :daily_rate, presence: true
+    belongs_to :venue
     belongs_to :organizer
 end
