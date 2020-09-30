7529c09a08dd139bd9af9b1b3fb78004e92afe08
justinmcla <jwmclawhorn@gmail.com>
2020-09-30 19:53:04 -0400

    builds new, create actions and some private methods

--- app/controllers/admin/inventories_controller.rb
+++ app/controllers/admin/inventories_controller.rb
@@ -1,2 +1,25 @@
 class Admin::InventoriesController < AdminController
+    before_action :set_venue
+    def new
+        @venue.inventories.build
+    end
+
+    def create
+        @inventory = @venue.inventories.build(post_params)
+        if @inventory.save
+            redirect_to admin_venue_path(@venue)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def set_venue
+        @venue = current_user.venues.find_by_id(params[:venue_id])
+    end
+
+    def post_params
+        params.permit(:name, :venue_id)
+    end
 end
