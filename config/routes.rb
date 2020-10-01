0132749c2e51e328c1a40d3747a72f19d45d9c92
justinmcla <jwmclawhorn@gmail.com>
2020-09-30 22:39:17 -0400

    adds task routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   namespace :admin do
+    resources :tasks,  only: [:new, :create, :edit, :update, :destroy]
     resources :organizers, :bookings
     resources :venues do
       resources :inventories do
