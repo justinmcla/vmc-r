cc4fcb208e94bd7ae508757c98c0a3d015272cfe
justinmcla <jwmclawhorn@gmail.com>
2020-09-30 22:36:35 -0400

    adds tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_021622) do
+ActiveRecord::Schema.define(version: 2020_10_01_023512) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "address_1"
@@ -88,6 +88,11 @@ ActiveRecord::Schema.define(version: 2020_10_01_021622) do
     t.integer "organizer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "notes"
   end
 
   create_table "users", force: :cascade do |t|
