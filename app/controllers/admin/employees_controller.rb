5c7c891eb8286ca669b453bb78d8b0f801d3db4a
justinmcla <jwmclawhorn@gmail.com>
2020-10-01 11:21:14 -0400

    adds new, create actions and strong params

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -2,4 +2,21 @@ class Admin::EmployeesController < AdminController
     def index
         @employees = current_user.employees.all 
     end
+    def new
+        @employee = current_user.employees.build
+    end
+    def create
+        @employee = current_user.employees.build(post_params)
+        if @employee.save
+            redirect_to admin_employees_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:employee).permit(:name, :start_date, :end_date, :active, :rate, :user_id)
+    end
 end
