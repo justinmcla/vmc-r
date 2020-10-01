869312b6afce42104f1cd6756629f6e94fc719df
justinmcla <jwmclawhorn@gmail.com>
2020-09-30 23:20:33 -0400

    adds route for venue show page for non-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :venues, only: [:show]
   namespace :admin do
     resources :tasks,  only: [:new, :create, :edit, :update, :destroy]
     resources :organizers, :bookings
