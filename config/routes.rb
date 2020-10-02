f4a042043c70d3605035b2768776907debe84556
justinmcla <jwmclawhorn@gmail.com>
2020-10-01 23:07:05 -0400

    adds omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     end
   end
   get '/admin/organizers/:id/add_poc', to: 'admin/organizers#add_poc', as: 'add_poc'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
   get '/home', to: 'admin#home'
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
