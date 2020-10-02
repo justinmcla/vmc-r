8b5362d56c022f0a683103cb0fe73517d3c3ff0d
justinmcla <jwmclawhorn@gmail.com>
2020-10-01 21:17:52 -0400

    adds validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
+    validates :state, inclusion: { in: %w(AL AK AZ AR CA CO 
+        CT DE FL GA HI ID 
+        IL IN IA KS KY LA 
+        ME MD MA MI MN MS 
+        MO MT NE NV NH NJ 
+        NM NY NC ND OH OK 
+        OR PA RI SC SD TN 
+        TX UT VT VA WA WV 
+        WI WY) }
+    validates :zip, format: { with: /(\d{5}\-\d{4}|\d{5})/, message: "must be either ##### or #####-####" }
     belongs_to :addressable, polymorphic: true
 end
