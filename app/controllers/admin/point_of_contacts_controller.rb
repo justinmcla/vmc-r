5e7f3914c3a067f3f127fceb18b622cf71321957
justinmcla <jwmclawhorn@gmail.com>
2020-10-08 13:14:58 -0400

    adds redirect to destroy action

--- app/controllers/admin/point_of_contacts_controller.rb
+++ app/controllers/admin/point_of_contacts_controller.rb
@@ -1,9 +1,10 @@
 class Admin::PointOfContactsController < AdminController
 
-    def organizer_point_of_contact_destroy
+    def destroy
         @organizer = current_user.organizers.find_by_id(params[:organizer_id])
         @point_of_contact = @organizer.point_of_contacts.find_by_id(params[:id])
         @point_of_contact.destroy
+        redirect_to admin_organizer_path(@organizer)
     end
     
 end
