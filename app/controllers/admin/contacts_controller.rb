8ed656786a94bcffa21def48f092597bb17b03f6
justinmcla <jwmclawhorn@gmail.com>
2020-10-08 13:14:31 -0400

    builds destroy action

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,2 +1,8 @@
 class Admin::ContactsController < AdminController
+    def destroy
+        @organizer = current_user.organizers.find_by_id(params[:organizer_id])
+        @contact = @organizer.contacts.find_by_id(params[:id])
+        @contact.destroy
+        redirect_to admin_organizer_path(@organizer)
+    end
 end
