6efa9cc945405956c08e452ed46d0b6c143147e2
justinmcla <jwmclawhorn@gmail.com>
2020-10-10 10:07:35 -0400

    adds validations to match schema

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+    validates :name, presence: true
+    validates :start_date, presence: true
+    validates :rate, presence: true, numericality: true
     belongs_to :user
     has_and_belongs_to_many :bookings
 end
