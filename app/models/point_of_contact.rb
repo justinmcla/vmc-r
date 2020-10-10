396ec005be16abd3f7c2a3ed32010272b998e16c
justinmcla <jwmclawhorn@gmail.com>
2020-10-10 10:15:43 -0400

    removes reject logic

--- app/models/point_of_contact.rb
+++ app/models/point_of_contact.rb
@@ -2,5 +2,5 @@ class PointOfContact < ApplicationRecord
     validates :name, presence: true
     belongs_to :organizer
     has_many :contacts, as: :contactable, dependent: :destroy
-    accepts_nested_attributes_for :contacts, reject_if: :blank_contact
+    accepts_nested_attributes_for :contacts
 end
