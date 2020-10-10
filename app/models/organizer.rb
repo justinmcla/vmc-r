3bc10a1416fd70ce8e5f2cc4eba8a46d894caf4c
justinmcla <jwmclawhorn@gmail.com>
2020-10-09 22:49:17 -0400

    adds organization type inclusion validation

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,5 +1,6 @@
 class Organizer < ApplicationRecord
     validates :name, presence: true
+    validates :organization_type, inclusion: { in: %w(Commercial Non-Profit Internal) }
     has_and_belongs_to_many :users
     has_many :point_of_contacts
     accepts_nested_attributes_for :point_of_contacts, reject_if: :blank_point_of_contact
