1ca18d70f00d8ac9124d75fa368efc5a07074094
justinmcla <jwmclawhorn@gmail.com>
2020-10-11 22:06:36 -0400

    creates omniauth controller and moves omniauth create to new controller

---
+++ app/controllers/omniauth_controller.rb
@@ -0,0 +1,14 @@
+class OmniauthController < ApplicationController
+    def create
+        @auth = request.env['omniauth.auth']
+        @user = User.find_by(email: @auth[:info][:email])
+        if @user
+          @user.google_uid ||= @auth[:uid]
+          session[:user_id] = @user.id
+        else
+          @user = User.create(name: @auth[:info], email: @auth[:info][:email], google_uid: @auth[:uid], password: @auth[:uid])
+          session[:user_id] = @user.id
+        end
+        redirect_to admin_path
+    end
+end
