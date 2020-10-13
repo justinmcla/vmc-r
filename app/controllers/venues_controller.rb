e924258b0053a5858369657f19005518f2f894f6
justinmcla <jwmclawhorn@gmail.com>
2020-10-12 20:33:25 -0400

    adds update action to handle booking query requests from potential clients

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,24 @@
 class VenuesController < ApplicationController    
+  before_action :set_venue
+
   def show
-    @venue = Venue.find_by(slug: params[:slug])
     @images = @venue.images
+  end
+
+  def update
+    @sender = {
+      name: params[:name],
+      email: params[:email],
+      message: params[:message]
+    }
+    @admin = @venue.user
+    VenueMailer.with(sender: @sender, admin: @admin, venue: @venue).booking_query.deliver_now
+    redirect_to venue_path(@venue)
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find_by(slug: params[:slug])
   end
 end
