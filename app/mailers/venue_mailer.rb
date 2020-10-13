7c0bca9560d77363be8ecfc3d8205de2f973baf0
justinmcla <jwmclawhorn@gmail.com>
2020-10-13 10:14:01 -0400

    adds logo as inline attachment

--- app/mailers/venue_mailer.rb
+++ app/mailers/venue_mailer.rb
@@ -3,6 +3,7 @@ class VenueMailer < ApplicationMailer
         @sender = params[:sender]
         @admin = params[:admin]
         @venue = params[:venue]
+        attachments.inline['VMC.png'] = File.read(Rails.root.join('app/assets/images/VMC.png'))
         mail(to: @admin.email, subject: 'Booking Query on VenueMC!')
     end
 end
