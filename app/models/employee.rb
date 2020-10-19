5eb4aeb426548bd29f13f64acfa30618ddb82988
justinmcla <jwmclawhorn@gmail.com>
2020-10-19 01:03:42 -0400

    adds active, inactive scopes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,10 @@ class Employee < ApplicationRecord
     validates :name, presence: true
     validates :start_date, presence: true
     validates :rate, presence: true, numericality: true
+
     belongs_to :user
     has_and_belongs_to_many :bookings
+
+    scope :active, -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
 end
