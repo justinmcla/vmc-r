32d00e59ec9c4cdb48cf829f8db6c13d251ddadc
justinmcla <jwmclawhorn@gmail.com>
2020-10-19 01:03:09 -0400

    adds display settings method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,17 @@
 module UsersHelper
-    def current_user
-        User.find_by(slug: session[:slug])
-    end
+    
+  def current_user
+    User.find_by(slug: session[:slug])
+  end
+
+  def display_settings
+    html = <<~HTML
+      <p>Name: #{current_user.name}</p>
+      <p>Email: #{current_user.email}</p>
+      <p>Member Since: #{current_user.created_at.strftime('%B %Y')}</p>
+    HTML
+
+    html.html_safe
+  end
+  
 end
