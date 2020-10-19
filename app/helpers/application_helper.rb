e1ec1c61eab1c3caa0d3d07aa09b49af71ed1963
justinmcla <jwmclawhorn@gmail.com>
2020-10-19 01:02:52 -0400

    adds display address method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def display_address(parent)
+
+    if parent.address
+
+      html = <<~HTML
+       <p>#{parent.address.address_1}<br>
+          #{parent.address.address_2 + '<br>' unless parent.address.address_2.empty? }
+          #{parent.address.city}, #{parent.address.state} #{parent.address.zip}</p>
+      HTML
+
+      html.html_safe
+
+    end
+
+  end
+
 end
