5d8db79164a9a81824a26d67d7fd77ee216b3686
justinmcla <jwmclawhorn@gmail.com>
2020-10-20 01:08:40 -0400

    adds scopes, instance methods

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -28,5 +28,32 @@ class Booking < ApplicationRecord
   scope :by_organizer, -> (organizer) { 
     joins(:organizer).where( "organizers.name LIKE ?", "%#{organizer}%" ) 
   }
+  scope :by_event_type, -> (event_type) {
+    where( "event_type LIKE ?", "%#{event_type}%")
+  }
+  scope :recurring,     -> { where(recurring: true) }
+  scope :catering,      -> { where(catering: true) }
+  scope :alcohol,       -> { where(alcohol: true) }
+  scope :lighting,      -> { where(lighting: true) }
+  scope :spotlight,     -> { where(spotlight: true) }
+  scope :sound,         -> { where(sound: true) }
+  scope :microphones,   -> { where(microphones: true) }
+  scope :security,      -> { where(security: true) }
+  scope :road_closure,  -> { where(road_closure: true) }
+  scope :contract,      -> { where(contract: true) }
+  scope :deposit,       -> { where(deposit: true) }
+  scope :paid,          -> { where(paid: true) } 
+
+  def contract_value
+    contract_value = self.daily_rate
+    self.employees.each do |employee|
+      contract_value += employee.rate * self.time_value
+    end
+    contract_value
+  end
+
+  def time_value
+    (self.exit_time - self.access_time) / 3600
+  end
 
 end
