27fc9f34c3832098b1a7981c8ca2da2152878383
justinmcla <jwmclawhorn@gmail.com>
2020-10-19 21:41:22 -0400

    removes obs methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
+
   self.abstract_class = true
 
-  def blank_address(attributes)
-    attributes[:address_1].blank?
-  end
-
-  def blank_contact(attributes)
-    attributes[:via].blank? && attributes[:value].blank?
-  end
 end
