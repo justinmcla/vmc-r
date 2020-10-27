88699ce6e6c24afcaf950ed78778b52273c70208
justinmcla <jwmclawhorn@gmail.com>
2020-10-27 11:54:19 -0400

    moved venues assignment to controller

--- app/controllers/admin/insights_controller.rb
+++ app/controllers/admin/insights_controller.rb
@@ -1,5 +1,6 @@
 class Admin::InsightsController < Admin::AdminController
   def index
     @bookings = params[:query].blank? ? current_user.bookings : current_user.bookings.by_event_type(params[:query])
+    @venues = current_user.venues
   end
 end
